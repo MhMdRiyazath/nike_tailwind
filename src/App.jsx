2426d75016babec233465796ff34eb72514d1650
MhMdRiyazath <mohamedriyazath.prof@gmail.com>
2024-03-03 12:48:50 +0530

    created all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-export default function App() {
+import React from 'react'
+
+const App=()=> {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='relative'>
+    nav
+    <section className='xl:padding-1 wide:padding-r padding-b'>
+      Hero
+    </section>
+    <section className='padding'>
+      Propulor product
+    </section>
+    <section className='padding'>
+      SuperQuality
+    </section>
+    <section className='padding-x py-10'>
+      Services
+    </section>
+    <section className='padding'>
+      SpecialOffers
+    </section>
+    <section className='bg-pale-blue padding'>
+      CustomersReviews
+    </section>
+    <section className='padding-x sm:py-32 py-16 w-full'>
+      Subscribe
+    </section>
+    <section className='bg-black padding-x padding-t pd-8'>
+      footer
+    </section>
+    
+    
+    </main>
   )
 }
+
+export default App
---
+++ src/components/PropularProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PropularProduct = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PropularProduct
