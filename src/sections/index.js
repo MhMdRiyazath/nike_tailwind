329c569d40682c5bf049784f7759eaaf590a032a
MhMdRiyazath <mohamedriyazath.prof@gmail.com>
2024-03-04 10:38:17 +0530

    completed nike

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,19 +1,19 @@
 import Hero from "./Hero";
-import PropularProduct from "./PropularProduct";
-import Subscribe from "./Subscribe";
+import PropularProduct from "./PopularProducts";
 import SuperQuality from "./SuperQuality";
 import Services from "./Services";
-import SpecialOffers from "./SpecialOffers";
-import Footer from "./Footer";
+import SpecialOffers from "./SpecialOffer";
 import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
 
 export {
     Hero,
     PropularProduct,
-    Subscribe,
     SuperQuality,
     Services,
     SpecialOffers,
-    Footer,
-    CustomerReviews
+    CustomerReviews,
+    Subscribe,
+    Footer
 }
